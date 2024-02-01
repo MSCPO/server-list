b1c5f6afeb1b722720712f8df0255a3b0cb66f44
peg195 <peg195@yandex.com>
2024-02-01 05:11:35 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center items-center h-[100vh]">
     <div>
       <div class="font-bold text-2xl">这里暂时还没有内容，也许以后会有吧？</div>
-      <a href="https://mcskin.cn" class="hover:underline">返回红石皮肤站</a>
+      <a href="javascript:history.back()" class="hover:underline">回到上一页</a>
     </div>
   </div>
 </template>
