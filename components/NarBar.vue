d0a1fa769a52d04ebd29a7b0a1a162242773de2d
peg195 <peg195@yandex.com>
2024-02-01 05:17:10 +0000

    Update NarBar.vue

--- components/NarBar.vue
+++ components/NarBar.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="shadow bg-transparent backdrop-blur fixed left-0 right-0 top-0">
     <div class="container mx-auto flex flex-row h-12 items-center px-2">
-      <div class="text-2xl font-thin">红石·找服</div>
+      <div class="text-2xl font-thin">dfgg服务器</div>
       <div class="flex-1"></div>
       <div>
-        <a href="https://mcskin.cn" class="hover:underline">返回红石皮肤站</a>
+        <a href="javascript:history.back()" class="hover:underline">返回上一页</a>
       </div>
     </div>
   </div>
