ba219f27bb16255119297e1240271203876476c2
peg195 <127033421+peg195@users.noreply.github.com>
2024-02-05 19:19:14 +0800

    Update NarBar.vue

--- components/NarBar.vue
+++ components/NarBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="shadow bg-transparent backdrop-blur fixed left-0 right-0 top-0">
     <div class="container mx-auto flex flex-row h-12 items-center px-2">
-      <div class="text-2xl font-thin">dfgg服务器</div>
+      <div class="text-2xl font-thin">MSCPO服务器宣传栏</div>
       <div class="flex-1"></div>
       <div>
         <a href="javascript:history.back()" class="hover:underline">返回上一页</a>
