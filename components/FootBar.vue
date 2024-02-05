239df0907b54d8c9d3699d63c13be31d2f9080ef
peg195 <127033421+peg195@users.noreply.github.com>
2024-02-05 19:16:07 +0800

    Update FootBar.vue

--- components/FootBar.vue
+++ components/FootBar.vue
@@ -2,7 +2,7 @@
   <!--footer-->
   <div class="black py-8">
     <div class="container mx-auto flex justify-center items-center">
-        <div class="text-md font-bold">© {{ year }} dfgg Studio</div>
+        <div class="text-md font-bold">© {{ year }} MSCPO-服务器集体宣传组织</div>
     </div>
   </div>
 </template>
